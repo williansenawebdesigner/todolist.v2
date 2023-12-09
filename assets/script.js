3789e3db959f8ee71a985d3815d384dd0e3aebc9
Willian Sena <williansena.webdesigner@gmail.com>
2023-12-08 22:59:15 -0300

    funcao para remover tarefas

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,29 @@ const inputTask = document.querySelector(".input-task")
 const btnTask = document.querySelector(".btn-task")
 // const listTask = document.querySelector(".list-task")
 
-//adicionar as tarefas
 
+//adicionar as tarefas
 document.querySelector(".btn-task").onclick = function() {
     if(document.querySelector(".inputs input").value.length == 0){
         alert("Você não pode adicionar uma tarefa vazia. Adicione um texto...")
     }
     else{
-        document.querySelector(".list-task").innerHTML += `
-            <li class="task-item"><i class="fa-regular fa-circle"></i>${document.querySelector(".input-task").value}<i class="fa-solid fa-x"></i></li>
+        document.querySelector(".list-task").innerHTML += 
         `
+            <li class="task-item"> <i class="fa-regular fa-circle"></i>
+                ${document.querySelector(".input-task").value} 
+                <i class="fa-solid fa-x delete"></i></li>
+           
+            
+        `
+
+        // remover tarefas
+        const current_task = document.querySelectorAll(".delete");
+        for(const i=0; i<current_task.length; i++){
+            current_task[i].onclick = function (){
+                this.parentNode.remove()
+            }
+        }
     }
 }
 
